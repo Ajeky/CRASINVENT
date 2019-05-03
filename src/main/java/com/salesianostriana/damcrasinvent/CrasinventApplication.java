551d836f1f20d5b10fc7136cdb903b539cc22df0
AlvaroMM <alvaromarquez98@gmail.com>
2019-05-03 14:20:32 +0200

    Proyecto recien creado
    Tan solo se han creado paquetes

---
+++ src/main/java/com/salesianostriana/damcrasinvent/CrasinventApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.damcrasinvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrasinventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrasinventApplication.class, args);
+	}
+
+}
