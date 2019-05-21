4dc306b37a9305c1f2a6f6860ffd515e18a4ace9
Alvaro <alvaromarquez98@gmail.com>
2019-05-21 19:04:22 +0200

    portada publica, data.sql simplificado

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,3 @@
-/*insert into Usuario (id, nombre, apellidos, email, nickname, password, telefono, cuentaCaducada, cuentaBloqueada, credencialesCaducadas) values (11111, 'Alvaro', 'Marquez Mata', 'alvaromarquez98@gmail.com', 'Ajeky', '1234', '615073398', false, false, false);
+insert into Usuario (id, nombre, apellidos, email, nickname, password, telefono, cuentaCaducada, cuentaBloqueada, credencialesCaducadas) values (11111, 'Alvaro', 'Marquez Mata', 'alvaromarquez98@gmail.com', 'Ajeky', '1234', '615073398', false, false, false);
 
 insert into Invent (id, nombre, usuario_id) values (11111, 'Asoka', 11111);
-insert into Invent (id, nombre, usuario_id) values (11112, 'Fixflex', 11111);
-insert into Invent (id, nombre, usuario_id) values (11113, 'Sonsing', 11111);
-insert into Invent (id, nombre, usuario_id) values (11114, 'Pannier', 11111);
-insert into Invent (id, nombre, usuario_id) values (11115, 'Stringtough', 11111);
-insert into Invent (id, nombre, usuario_id) values (11116, 'Cardify', 11111);
-insert into Invent (id, nombre, usuario_id) values (11117, 'Keylex', 11111);
-insert into Invent (id, nombre, usuario_id) values (11118, 'Home Ing', 11111);
-insert into Invent (id, nombre, usuario_id) values (11119, 'Flowdesk', 11111);
-insert into Invent (id, nombre, usuario_id) values (111110, 'Redhold', 11111);*/
